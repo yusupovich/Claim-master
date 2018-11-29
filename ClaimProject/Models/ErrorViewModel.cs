8a5fb37de46dd9b678ac7ba33d3d4781136e503d
Abdullo <abdullo@MMCenter.uz>
2018-11-29 16:21:26 +0500

    Добавьте файлы проекта.

---
+++ ClaimProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClaimProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
